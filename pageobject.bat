bb87e00e3142cf6ea8a7c8c536e64326c0e5a428
dona001 <slugendra@gmail.com>
2018-11-27 23:53:59 +0530

    commit

---
+++ pageobject.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Suve\eclipse\pageobject\ 
+java -cp C:\Users\Suve\eclipse\pageobject\lib\*;C:\Users\Suve\eclipse\pageobject\target\classes\ org.testng.TestNG TestNG.xml
---
+++ src/main/java/Excercise/pageobject/App.java
@@ -0,0 +1,13 @@
+package Excercise.pageobject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
